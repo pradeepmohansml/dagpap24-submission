563d8c1516838c0df855e42f522ad6babb36e2c8
Pradeep <pmohans@outlook.com>
2024-04-25 23:49:29 +0000

    local changes pradeep

--- notebooks/fileconfig.py
+++ notebooks/fileconfig.py
@@ -2,30 +2,29 @@ BASELINE="scibert"
 BASELINE_CONFIG_FILE_NAME="config_baseline.yml"
 DISTILBERT_CONFIG_FILE_NAME="config_distilbert.yml"
 ROBERTA_CONFIG_FILE_NAME="config_roberta.yml"
-BIOGPT_CONFIG_FILE_NAME="config_biogpt.yml"
+BIOMED_ROBERTA_CONFIG_FILE_NAME="config_biomed_roberta.yml"
 T5_CONFIG_FILE_NAME="config_T5.yml"
-PHI_CONFIG_FILE_NAME="config_phi.yml"
 OPENAI_CONFIG_FILE_NAME="config_openai_gpt2.yml"
-FALCON_CONFIG_FILE_NAME="config_falcon.yml"
+DEBERTA_CONFIG_FILE_NAME="config_deberta.yml"
+SCIBERT_CONFIG_FILE_NAME = "config_scibert.yml"
 
 BASELINE_CONFIG_JSON="config_baseline.json"
+SCIBERT_CONFIG_JSON = "config_scibert.json"
 DISTILBERT_CONFIG_JSON="config_distilbert.json"
 ROBERTA_CONFIG_JSON="config_roberta.json"
-BIOGPT_CONFIG_JSON="config_biogpt.json"
+BIOMED_ROBERTA_CONFIG_JSON="config_biomed_roberta.json"
 T5_CONFIG_JSON="config_T5.json"
-PHI_CONFIG_JSON="config_phi.json"
+DEBERTA_CONFIG_JSON="config_deberta.json"
 OPENAI_CONFIG_JSON="config_openai_gpt2.json"
-FALCON_CONFIG_JSON="config_falcon.json"
 
 CANDIDATE_CONFIG_FILES = {
-                          "scibert":BASELINE_CONFIG_FILE_NAME,
+                          "scibert":SCIBERT_CONFIG_FILE_NAME,
                           "distilbert":DISTILBERT_CONFIG_FILE_NAME,
                           "roberta":ROBERTA_CONFIG_FILE_NAME,
-                          "biogpt":BIOGPT_CONFIG_FILE_NAME,
+                           "biomed_roberta": BIOMED_ROBERTA_CONFIG_FILE_NAME,
+                           "deberta": DEBERTA_CONFIG_FILE_NAME,
                           "t5": T5_CONFIG_FILE_NAME,
-                          "phi":PHI_CONFIG_FILE_NAME,
                           "openaigpt2":OPENAI_CONFIG_FILE_NAME,
-                          "falcon":FALCON_CONFIG_FILE_NAME
                          }
 
 JUDGES=["GPT35_TURBO","SETFIT_GBC"]
